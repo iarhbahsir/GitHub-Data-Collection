fd3c8b9db697e649b8df410192d39e86e4df74cb
iarhbahsir <rrai.squared@utexas.edu>
2017-05-31 11:33:20 -0500

    Added TodoReader
    Added TodoReader

---
+++ TodoReader.java
@@ -0,0 +1,14 @@
+package todoReader;
+import com.jcabi.github.*;
+public class TodoReader {
+
+	public static void main(String[] args) 
+	{
+		//read in Todo.txt file from given GitHub repository and give pertinent information
+		//v1: list total number of tasks (lines)
+		
+		Github gt = new RtGithub();
+		
+	}
+
+}
