eaf2da0de921e94714aa02f4b4aafec75cb0780b
iarhbahsir <rrai.squared@utexas.edu>
2017-05-31 18:22:21 -0500

    Goes through files in directories
    Recursively checks TODO comments/TODO.txt in directories

--- TodoReader.java
+++ TodoReader.java
@@ -2,9 +2,67 @@ package todoReader;
 import java.io.IOException;
 import java.util.Scanner;
 
-import com.jcabi.github.*;
-public class TodoReader {
+import javax.json.Json;
 
+import com.jcabi.github.*;
+public class TodoReader 
+{
+	
+	static int numTasks = 0;
+	
+	private static void printContents(Contents cont, String path, String ref)
+	{
+		try {
+			for(Content co: cont.iterate(path, ref))
+			{
+				Content.Smart c = new Content.Smart(co);
+				//System.out.println(c.path());
+				try 
+				{
+					c.json();
+				} 
+				catch (javax.json.JsonException e) 
+				{
+					if(c.path().contains("/"))
+					{
+						//System.out.println(path + "/" + c.path().substring((c.path().lastIndexOf("/") + 1)));
+						printContents(cont, path + "/" + c.path().substring((c.path().lastIndexOf("/")) + 1), "master");
+					}
+					
+					else
+					{
+						//System.out.println(path + "/" + c.path());
+						printContents(cont, path + "/" + c.path(), "master");
+					}
+					
+				}
+				Scanner fileScanner = new Scanner(c.raw());
+				while(fileScanner.hasNextLine())
+				{
+					String nextLine = fileScanner.nextLine();
+					if(!c.path().equals("TODO.txt"))
+					{
+						if(nextLine.contains("//"))
+						{
+							if(nextLine.indexOf("TODO") > nextLine.indexOf("//"))
+							{
+								System.out.println(nextLine);
+								numTasks++;
+							}
+						}
+					}
+					else
+					{
+						System.out.println(nextLine);
+						numTasks++;
+					}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+	}
+	
 	public static void main(String[] args) throws IOException 
 	{
 		//read in Todo.txt file from given GitHub repository and give pertinent information
@@ -18,39 +76,9 @@ public class TodoReader {
 		String repoName = kb.nextLine();
 		//iarhbahsir/GitHub-Data-Collection
 		Repo rp = gh.repos().get(new Coordinates.Simple(repoName));
-		Contents cont = rp.contents();
-		for(Content c: cont.iterate("", "master"))
-		{
-			
-			System.out.println(c.path());
-			Scanner fileScanner = new Scanner(c.raw());
-			while(fileScanner.hasNextLine())
-			{
-				String nextLine = fileScanner.nextLine();
-				if(!c.path().equals("TODO.txt"))
-				{
-					if(nextLine.contains("//"))
-					{
-						if(nextLine.indexOf("TODO") > nextLine.indexOf("//"))
-						System.out.println(nextLine);
-					}
-				}
-				else
-				{
-					System.out.println(nextLine);
-				}
-			}
-		}
-		/*Scanner td = new Scanner(cont.get("TODO.txt").raw());
-		int numTasks = 0;
-		
-		while(td.hasNextLine())
-		{
-			numTasks++;
-			System.out.println(td.nextLine());
-		}
-		
-		System.out.println("\nTotal number of tasks: " + numTasks);*/
+		Contents repoCont = rp.contents();
+		printContents(repoCont, "", "master");
+		System.out.println("\nTotal number of tasks: " + numTasks);
 	}
 
 }
