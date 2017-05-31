647d9a4696c3896bf50c1403e63e4b06657f8a89
iarhbahsir <rrai.squared@utexas.edu>
2017-05-31 12:36:37 -0500

    Basic Counter
    Returns the number of files in a repo

--- TodoReader.java
+++ TodoReader.java
@@ -1,14 +1,28 @@
 package todoReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 import com.jcabi.github.*;
 public class TodoReader {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws IOException 
 	{
 		//read in Todo.txt file from given GitHub repository and give pertinent information
-		//v1: list total number of tasks (lines)
+		//v1: list total number of tasks
 		
-		Github gt = new RtGithub();
+		Github gh = new RtGithub();
+		Repo rp = gh.repos().get(new Coordinates.Simple("iarhbahsir/GitHub-Data-Collection"));
+		Contents cont = rp.contents();
+		Scanner td = new Scanner(cont.get("TODO.txt").raw());
+		int numTasks = 0;
 		
+		while(td.hasNextLine())
+		{
+			numTasks++;
+			System.out.println(td.nextLine());
+		}
+		
+		System.out.println("\nTotal number of tasks: " + numTasks);
 	}
 
 }
